8379a9ce7e894170eb1d4f8e5aa8235e2bbfe6ab
Ashar <yournewemail@gmail.com>
2025-11-27 19:29:15 +0500

    run ci/cd

--- devops_project/src/components/Contact.jsx
+++ devops_project/src/components/Contact.jsx
@@ -95,7 +95,7 @@ const Contact = forwardRef((props, ref) => {
 
           <div className="text-gray-700 dark:text-gray-300 text-sm">
             <p><strong>Email:</strong> devOps@gmail.com.pk</p>
-            <p><strong>Phone:</strong> +92 306-112-0990</p>
+            <p><strong>Phone:</strong> +92 300-112-0990</p>
             <p><strong>Address:</strong> Pakistan</p>
           </div>
         </div>
